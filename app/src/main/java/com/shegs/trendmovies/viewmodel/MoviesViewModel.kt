9fbf2d007018401f8240288aaac32dd30cb6b186
Oladokun oluwasegun <oladokunoluwasegun@gmail.com>
2023-07-31 04:36:04 +0100

    ViewModel created

--- app/src/main/java/com/shegs/trendmovies/viewmodel/MoviesViewModel.kt
+++ app/src/main/java/com/shegs/trendmovies/viewmodel/MoviesViewModel.kt
@@ -1,13 +1,33 @@
 package com.shegs.trendmovies.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.shegs.trendmovies.model.repositories.MoviesRepository
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 class MoviesViewModel : ViewModel(){
     private val moviesRepository = MoviesRepository()
 
-    private val _movies = MutableStateFlow()
+    private val _moviesState = MutableStateFlow<MoviesState>(MoviesState.Loading)
+    val moviesState: StateFlow<MoviesState> = _moviesState
+
+    init {
+        fetchPopularMovies()
+    }
+
+    private fun fetchPopularMovies() {
+        viewModelScope.launch {
+            try {
+                val movies = moviesRepository.getPopularMovies()
+                _moviesState.value = MoviesState.Success(movies)
+            }catch (e: Exception){
+                _moviesState.value = MoviesState.Error("Error Fetching Movies: $(e.message)")
+            }
+        }
+
+    }
 
 
 }
